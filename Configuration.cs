cef93caad54a60f1fa95d4e109e60607557be490
羽学 <1242509682@qq.com>
2024-03-19 17:38:31 +0800

    添加项目文件。

---
+++ Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TShockAPI;
+using static OTAPI.Hooks;
+
+namespace 禁地表弹幕
+{
+    public class Configuration
+    {
+        public static readonly string FilePath = Path.Combine(TShock.SavePath, "禁地表弹幕表.json");
+        public int[] 禁用地表弹幕id = new int[] { 28, 29, 37, 65, 68, 99, 108, 136, 137, 138, 139, 142, 143, 144, 146, 147, 149, 164, 339, 341, 354, 453, 516, 519, 637, 716, 718, 727, 773, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 796, 797, 798, 799, 800, 801, 804, 805, 806, 807, 809, 810, 863, 868, 869, 904, 905, 906, 910, 911, 949, 1013, 1014 };
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(str);
+                }
+            }
+        }
+
+        public static Configuration Read(string path)
+        {
+            if (!File.Exists(path))
+                return new Configuration();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var cf = JsonConvert.DeserializeObject<Configuration>(sr.ReadToEnd());
+                    return cf;
+                }
+            }
+        }
+    }
+}
---
+++ 禁地表弹幕.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace 禁地表弹幕
+{
+    [ApiVersion(2, 1)]
+    public class Surfaceprojban : TerrariaPlugin
+    {
+        public override string Author => "羽学 Q群644435865 感谢Cai 西江小子";
+        public override string Description => "禁止特定弹幕在地表产生";
+        public override string Name => "禁地表弹幕";
+        public override Version Version => new(1, 0, 0, 3);
+        internal static Configuration Config;
+        public Surfaceprojban(Main game) : base(game)
+        {
+            Order = 40;
+        }
+        public override void Initialize()
+        {
+            LoadConfig();
+            GetDataHandlers.NewProjectile += OnProjectileNew;
+            GeneralHooks.ReloadEvent += ReloadConfig;
+        }
+        private static void LoadConfig()
+        {
+            Config = Configuration.Read(Configuration.FilePath);
+            Config.Write(Configuration.FilePath);
+        }
+
+        private static void ReloadConfig(ReloadEventArgs args)
+        {
+            LoadConfig();
+            args.Player?.SendSuccessMessage("[{0}]重新加载配置完毕。", typeof(Surfaceprojban).Name);
+        }
+        //禁止生成的弹幕
+        private static readonly HashSet<int> restrictedProjectiles = new HashSet<int>();
+        private void OnProjectileNew(object sender, GetDataHandlers.NewProjectileEventArgs e)
+        {
+            if (e.Player.HasPermission("免检地表弹幕")) //插件权限名
+                return;
+
+            //调用Config里的弹幕ID，如果产生这些弹幕ID，则根据地图Y轴小于地表范围作为参考范围，清除这些弹幕。
+            if ((Config.禁用地表弹幕id.Contains(e.Type) || restrictedProjectiles.Contains(e.Type)) && e.Position.Y < Main.worldSurface * 16) 
+            {
+                e.Player.RemoveProjectile(e.Identity, e.Owner);
+                e.Handled = true;
+            }
+        }
+    }
+}
